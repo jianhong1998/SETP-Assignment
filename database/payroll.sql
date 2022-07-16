ba490a13730ef5932a813fcec30d3d11e87715f3
Jian Hong <77224948+jianhong1998@users.noreply.github.com>
2022-07-16 11:31:17 +0800

    First upload
    First upload of the program

---
+++ database/payroll.sql
@@ -0,0 +1,408 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 08, 2021 at 09:38 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `payroll`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `allowances`
+--
+
+CREATE TABLE `allowances` (
+  `id` int(30) NOT NULL,
+  `allowance` text NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `allowances`
+--
+
+INSERT INTO `allowances` (`id`, `allowance`, `description`) VALUES
+(1, 'Transport', 'Transport Allowance'),
+(2, 'Mobile', 'Mobile Allowance'),
+(3, 'Meal', 'Meal Allowance'),
+(4, 'OT', 'OT Allowance'),
+(5, 'Medical', 'Medical Allowance'),
+(6, 'Flexi', 'Flexi Allowance'),
+(7, 'Petrol', 'Petrol Allowance');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `id` int(11) NOT NULL,
+  `employee_id` int(20) NOT NULL,
+  `log_type` tinyint(1) NOT NULL COMMENT '1 = AM IN,2 = AM out, 3= PM IN, 4= PM out\r\n',
+  `datetime_log` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `attendance`
+--
+
+INSERT INTO `attendance` (`id`, `employee_id`, `log_type`, `datetime_log`, `date_updated`) VALUES
+(10, 9, 1, '2022-06-16 08:00:00', '2022-06-29 16:16:57'),
+(11, 9, 2, '2022-06-16 12:00:00', '2022-06-29 16:16:57'),
+(12, 9, 3, '2022-06-16 13:00:00', '2022-06-29 16:16:57'),
+(16, 9, 4, '2022-05-16 17:00:00', '2022-06-29 16:16:57'),
+(17, 9, 1, '2022-05-07 09:00:00', '2022-06-29 15:10:07'),
+(18, 9, 2, '2022-03-07 11:00:00', '2022-06-29 15:11:06');
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `deductions`
+--
+
+CREATE TABLE `deductions` (
+  `id` int(30) NOT NULL,
+  `deduction` text NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `deductions`
+--
+
+INSERT INTO `deductions` (`id`, `deduction`, `description`) VALUES
+(1, 'Cash Advance', 'Cash Advance'),
+(3, 'Sample', 'Sample Deduction');
+
+-- --------------------------------------------------------
+
+--
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`id`, `name`) VALUES
+(1, 'IT Department'),
+(2, 'HR Department'),
+(3, 'Accounting and Finance Department'),
+(4, 'HSE Department'),
+(5, 'Security Department'),
+(6, 'Sales Department');
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+
+CREATE TABLE `employee` (
+  `id` int(20) NOT NULL,
+  `employee_no` varchar(100) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `department_id` int(30) NOT NULL,
+  `designation_id` int(30) NOT NULL,
+  `salary` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `employee`
+--
+
+INSERT INTO `employee` VALUES
+(9, '2020-9838', 'Jian Hong', 'Lee', 1, 1, 30000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee_allowances`
+--
+
+CREATE TABLE `employee_allowances` (
+  `id` int(30) NOT NULL,
+  `employee_id` int(30) NOT NULL,
+  `allowance_id` int(30) NOT NULL,
+  `type` tinyint(1) NOT NULL COMMENT '1 = Monthly, 2= Semi-Montly, 3 = once',
+  `amount` float NOT NULL,
+  `effective_date` date NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `employee_allowances`
+--
+
+INSERT INTO `employee_allowances` (`id`, `employee_id`, `allowance_id`, `type`, `amount`, `effective_date`, `date_created`) VALUES
+(1, 9, 4, 1, 1000, '0000-00-00', '2020-09-29 11:20:04'),
+(3, 9, 3, 2, 300, '0000-00-00', '2020-09-29 11:37:31'),
+(5, 9, 1, 3, 1000, '2020-09-16', '2020-09-29 11:38:31');
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `employee_deductions`
+--
+
+CREATE TABLE `employee_deductions` (
+  `id` int(30) NOT NULL,
+  `employee_id` int(30) NOT NULL,
+  `deduction_id` int(30) NOT NULL,
+  `type` tinyint(1) NOT NULL COMMENT '1 = Monthly, 2= Semi-Montly, 3 = once',
+  `amount` float NOT NULL,
+  `effective_date` date NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `employee_deductions`
+--
+
+INSERT INTO `employee_deductions` (`id`, `employee_id`, `deduction_id`, `type`, `amount`, `effective_date`, `date_created`) VALUES
+(2, 9, 3, 2, 500, '0000-00-00', '2020-09-29 11:52:46'),
+(3, 9, 1, 3, 1500, '2020-09-16', '2020-09-29 11:53:27');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payroll`
+--
+
+CREATE TABLE `payroll` (
+  `id` int(30) NOT NULL,
+  `ref_no` text NOT NULL,
+  `date_from` date NOT NULL,
+  `date_to` date NOT NULL,
+  `type` tinyint(1) NOT NULL COMMENT '1 = monthly ,2 Weekly',
+  `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0 =New,1 = computed',
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `payroll`
+--
+
+INSERT INTO `payroll` (`id`, `ref_no`, `date_from`, `date_to`, `type`, `status`, `date_created`) VALUES
+(1, '2020-3543', '2020-09-16', '2020-09-30', 2, 1, '2020-09-29 15:04:13');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payroll_items`
+--
+
+CREATE TABLE `payroll_items` (
+  `id` int(30) NOT NULL,
+  `payroll_id` int(30) NOT NULL,
+  `employee_id` int(30) NOT NULL,
+  `present` int(30) NOT NULL,
+  `absent` int(10) NOT NULL,
+  `late` text NOT NULL,
+  `salary` double NOT NULL,
+  `allowance_amount` double NOT NULL,
+  `allowances` text NOT NULL,
+  `net` int(11) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `payroll_items`
+--
+
+INSERT INTO `payroll_items` (`id`, `payroll_id`, `employee_id`, `present`, `absent`, `late`, `salary`, `allowance_amount`, `allowances`, `net`, `date_created`) VALUES
+(10, 1, 9, 1, 10, '0', 30000, 1300, '[{\"aid\":\"3\",\"amount\":\"300\"},{\"aid\":\"1\",\"amount\":\"1000\"}]', 664, '2020-09-29 18:46:59');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `designation`
+--
+
+CREATE TABLE `designation` (
+  `id` int(30) NOT NULL,
+  `department_id` int(30) NOT NULL,
+  `name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `designation`
+--
+
+INSERT INTO `designation` (`id`, `department_id`, `name`) VALUES
+(1, 1, 'Programmer'),
+(2, 2, 'HR Supervisor'),
+(3, 3, 'Accounting Clerk'),
+(4, 4, 'HR Officer'),
+(5, 5, 'Security Officer'),
+(6, 6, 'Sales Executive'),
+(7, 1, 'Scrum Master'),
+(8, 1, 'Product Owner');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(30) NOT NULL,
+  `doctor_id` int(30) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `address` text NOT NULL,
+  `contact` text NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 2 COMMENT '1=admin , 2 = staff'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `doctor_id`, `name`, `address`, `contact`, `username`, `password`, `type`) VALUES
+(1, 0, 'Administrator', '', '', 'admin', '11', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `allowances`
+--
+ALTER TABLE `allowances`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `employee`
+--
+ALTER TABLE `employee`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `employee_allowances`
+--
+ALTER TABLE `employee_allowances`
+  ADD PRIMARY KEY (`id`);
+
+--
+--
+-- Indexes for table `payroll`
+--
+ALTER TABLE `payroll`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `payroll_items`
+--
+ALTER TABLE `payroll_items`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `designation`
+--
+ALTER TABLE `designation`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `allowances`
+--
+ALTER TABLE `allowances`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `employee`
+--
+ALTER TABLE `employee`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `employee_allowances`
+--
+ALTER TABLE `employee_allowances`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `payroll`
+--
+ALTER TABLE `payroll`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `payroll_items`
+--
+ALTER TABLE `payroll_items`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `designation`
+--
+ALTER TABLE `designation`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
